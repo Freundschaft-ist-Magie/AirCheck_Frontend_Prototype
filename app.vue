56d432e5b1af22945114684f5d19730fff339b56
Justin <justin-urbanek@bluewin.ch>
2025-03-20 09:22:16 +0100

    feat: setup routeing

--- app.vue
+++ app.vue
@@ -1,14 +1,11 @@
 <script lang="ts" setup>
-import Button from 'primevue/button';
 </script>
 
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button label="Submit" />
-
+    <NuxtLayout>
+      <NuxtLoadingIndicator :color="'#fff'" />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button label="Submit" />
+
+  </div>
+</template>
