4bf71c783845165d2417c5ea846ae7370a011925
Justin <justin-urbanek@bluewin.ch>
2025-03-20 09:19:05 +0100

    feat: setup primevue

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
+<script lang="ts" setup>
+import Button from 'primevue/button';
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button label="Submit" />
+
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,35 @@
 import tailwindcss from "@tailwindcss/vite";
+import Aura from '@primeuix/themes/aura';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+
+  // https://tailwindcss.com/docs/installation/framework-guides/nuxt
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
+  // https://primevue.org/nuxt/
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    }
+  }
 })
