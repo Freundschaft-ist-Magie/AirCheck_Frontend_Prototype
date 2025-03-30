6edcf87d4dcc5e20bd57e2a03d00b546cebbec85
egomann88 <justin-urbanek@bluewin.ch>
2025-03-30 20:35:05 +0200

    feat: dissource status cards to component

---
+++ components/StatisticCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps({
+  title: String,
+  text: Number,
+  normalRange: String,
+});
+</script>
+
+<template>
+  <div class="bg-white p-4 shadow-md rounded-md">
+    <h3 class="font-bold text-primary2">{{ title }}</h3>
+    <p class="text-2xl font-bold">{{ text }} °C</p>
+    <p class="text-gray-500">Normaler Bereich: {{ normalRange }}</p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,10 @@
 <script lang="ts" setup>
-const temperature = ref(20);
-const humidity = ref(45);
-const co2 = ref(660);
-
-const isServerOnline = ref(false);
-
-const roomName = ref("Room E20");
 const latestFetch = ref(new Date());
 const statusMapping = [
   { status: "Alles ok", codes: [200, 201, 204] },
   { status: "Fast ok", codes: [400, 401, 403, 404] },
   { status: "Alles schlecht", codes: [500, 502, 503] },
 ];
-
-const normals = {
-  temperature: { min: 20, max: 25 },
-  humidity: { min: 30, max: 60 },
-  co2: { max: 1000 },
-};
 
 const selectedRoom = ref();
 const rooms = ref([
@@ -59,31 +46,6 @@ function generateStatusText(statusCode: number) {
   return status ? status.status : "unknown";
 }
 
-function calcNormals(value: number, type: keyof typeof normals) {
-  const normal = normals[type];
-
-  if (normal.min !== undefined && value < normal.min) {
-    return "text-blue-500";
-  } else if (normal.max !== undefined && value > normal.max) {
-    return "text-red-500";
-  }
-
-  return "text-black";
-}
-
-function calcNormalRangeText(type: keyof typeof normals) {
-  const normal = normals[type];
-  if (normal.min !== undefined && normal.max !== undefined) {
-    return `${normal.min} - ${normal.max}`;
-  } else if (normal.min !== undefined) {
-    return `> ${normal.min}`;
-  } else if (normal.max !== undefined) {
-    return `< ${normal.max}`;
-  }
-
-  return "unknown";
-}
-
 const chartData = ref({
   labels: ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"],
   datasets: [
@@ -104,6 +66,24 @@ const chartOptions = ref({
     },
   },
 });
+
+const cards = ref([
+  {
+    title: "Temperatur",
+    text: "20 °C",
+    normalRange: "20 - 25 °C",
+  },
+  {
+    title: "Luftfeuchtigkeit",
+    text: "45 %",
+    normalRange: "30 - 60 %",
+  },
+  {
+    title: "CO2 Level",
+    text: "660 ppm",
+    normalRange: "< 2000 ppm",
+  },
+]);
 </script>
 
 <template>
@@ -125,29 +105,13 @@ const chartOptions = ref({
   </div>
 
   <div class="mt-4 grid grid-cols-3 gap-4">
-    <div class="bg-white p-4 shadow-md rounded-md">
-      <h3 class="font-bold text-primary2">Temperatur</h3>
-      <p class="text-2xl font-bold" :class="calcNormals(temperature, 'temperature')">
-        {{ temperature }} &deg;C
-      </p>
-      <p class="text-gray-500">
-        Normaler Bereich: {{ calcNormalRangeText("temperature") }} &deg;C
-      </p>
-    </div>
-    <div class="bg-white p-4 shadow-md rounded-md">
-      <h3 class="font-bold text-primary2">Humidity</h3>
-      <p class="text-2xl font-bold" :class="calcNormals(humidity, 'humidity')">
-        {{ humidity }} %
-      </p>
-      <p class="text-gray-500">
-        Normaler Bereich: {{ calcNormalRangeText("humidity") }} %
-      </p>
-    </div>
-    <div class="bg-white p-4 shadow-md rounded-md">
-      <h3 class="font-bold text-primary2">CO2 Level</h3>
-      <p class="text-2xl font-bold" :class="calcNormals(co2, 'co2')">{{ co2 }} ppm</p>
-      <p class="text-gray-500">Normaler Bereich: {{ calcNormalRangeText("co2") }} ppm</p>
-    </div>
+    <StatisticCard
+      v-for="card in cards"
+      :key="card.title"
+      :title="card.title"
+      :text="card.text"
+      :normalRange="card.normalRange"
+    />
   </div>
 
   <div class="mt-4 grid grid-cols-3 gap-4">
