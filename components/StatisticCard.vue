25f858871d77b33e67c6b74f6837c5f1cab10a97
egomann88 <justin-urbanek@bluewin.ch>
2025-03-30 20:39:45 +0200

    fix: typo

--- components/StatisticCard.vue
+++ components/StatisticCard.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <div class="bg-white p-4 shadow-md rounded-md">
     <h3 class="font-bold text-primary2">{{ title }}</h3>
-    <p class="text-2xl font-bold">{{ text }} °C</p>
+    <p class="text-2xl font-bold">{{ text }}</p>
     <p class="text-gray-500">Normaler Bereich: {{ normalRange }}</p>
   </div>
 </template>
