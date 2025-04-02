40670e5a229eeec30c75e4ed3b4ded12fe7fde51
egomann88 <justin-urbanek@bluewin.ch>
2025-04-02 21:01:09 +0200

    feat: dissource RoomSelectorCard

---
+++ components/RoomSelectorCard.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import Room from "~/models/Room";
+
+defineProps<{
+  latestFetch: Date;
+  selectedRoom: Room | null;
+  rooms: Room[];
+}>();
+
+function formatDate(date: Date) {
+  return date.toLocaleString("de-CH", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+</script>
+
+<template>
+  <div class="mt-4 p-4 bg-white shadow-md rounded-md flex justify-between items-center">
+    <div>
+      <room-selector
+        :options="rooms"
+        :placeholder="'Raum auswählen'"
+        :filter-field="'name'"
+        :selected="selectedRoom"
+      />
+      <p class="text-gray-500">Zuletzt Aktualisiert: {{ formatDate(latestFetch) }}</p>
+    </div>
+  </div>
+</template>
