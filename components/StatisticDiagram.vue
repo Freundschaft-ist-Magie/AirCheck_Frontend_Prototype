a479877f09b890fbc674adbdc92899664722cda6
Jyods <jyodi.sprenger@hotmail.ch>
2025-04-10 12:19:25 +0200

    feat: added history

--- components/StatisticDiagram.vue
+++ components/StatisticDiagram.vue
@@ -6,12 +6,13 @@ defineProps<{
   title: string;
   chartData: ChartData;
   chartOptions: ChartOptions;
+  chartType: string;
 }>();
 </script>
 
 <template>
   <div class="bg-white p-4 shadow-md rounded-md">
     <h3 class="font-bold text-primary2 text-center">{{ title }}</h3>
-    <Chart type="line" :data="chartData" :options="chartOptions" />
+    <Chart :type="chartType" :data="chartData" :options="chartOptions" />
   </div>
 </template>
