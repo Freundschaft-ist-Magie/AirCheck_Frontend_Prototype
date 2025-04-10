a6138e784fe8b303df83ea7a61df595c0dab73d4
Maxoso41 <40214662+Maxoso41@users.noreply.github.com>
2025-04-10 10:25:28 +0200

    feat: enhance header with user data and notification panel

--- components/MainLayout/Header.vue
+++ components/MainLayout/Header.vue
@@ -2,11 +2,21 @@
 import { useAuthStore } from "~/utils/stores/base/AuthStore";
 
 const authStore = useAuthStore();
-const popoverPanel = ref();
+const navLinksPanel = ref();
+const notifPanel = ref();
 const isServerOnline = ref(false);
+const userData = ref({
+  // this will contain the current user's data
+  name: "Max Mustermann",
+  image: "https://avatars.githubusercontent.com/u/143788063?s=200&v=4",
+});
 
-const togglePopover = (event: Event) => {
-  popoverPanel.value.toggle(event);
+const toggleNavLinksPanel = (event: Event) => {
+  navLinksPanel.value.toggle(event);
+};
+
+const toggleNotifPanel = (event: Event) => {
+  notifPanel.value.toggle(event);
 };
 
 const panelLinks = computed(() => {
@@ -50,15 +60,36 @@ const panelLinks = computed(() => {
 
       <div class="flex gap-2 items-center">
         <Button
-          icon="pi pi-ellipsis-v"
+          icon="pi pi-bell"
           severity="secondary"
-          rounded
-          @click="togglePopover"
+          class="bg-transparent! border-0! rounded-full!"
+          @click="toggleNotifPanel"
           aria-haspopup="true"
-          aria-controls="popoverPanel"
+          aria-controls="notifPanel"
         />
+        <Button
+          severity="secondary"
+          class="p-0 bg-transparent! border-0!"
+          @click="toggleNavLinksPanel"
+          aria-haspopup="true"
+          aria-controls="navLinksPanel"
+        >
+          <img
+            class="w-10 h-10 rounded-full"
+            :src="userData.image"
+            :alt="userData.name"
+          />
+          <p>{{ userData.name }}</p>
+          <i class="pi pi-chevron-down text-sm" />
+        </Button>
 
-        <OverlayPanel ref="popoverPanel" id="popoverPanel">
+        <OverlayPanel ref="notifPanel" id="notifPanel">
+          <div class="flex flex-col gap-2 p-2">
+            <span>NOT IMPLEMENTED.</span>
+          </div>
+        </OverlayPanel>
+
+        <OverlayPanel ref="navLinksPanel" id="navLinksPanel">
           <div class="flex flex-col gap-2 p-2">
             <NuxtLink
               v-for="link in panelLinks"
